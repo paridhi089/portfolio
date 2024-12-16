383f205d5589de8b0f7d042c5a9175e2b633b665
Paridhi Prisha Saikia <paridhiprishasaikia@gmail.com>
2024-12-16 21:08:13 +0530

    Updated UI

--- client/src/pages/ShowProject.jsx
+++ client/src/pages/ShowProject.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import supabase from "../supabase/supabaseClient";
 import Nav from "../components/Nav";
+import LinearProgress from '@mui/joy/LinearProgress';
 
 const ShowProject = () => {
   const { id } = useParams();
@@ -31,7 +32,17 @@ const ShowProject = () => {
     fetchProject();
   }, [id]);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="flex-col">
+        <LinearProgress className=" w-[200px] sm:w-[700px]" color="neutral" size="lg"/>
+        <p className=" text-center text-white">Loading...</p>
+
+        </div>
+       
+      </div>
+    );
   if (error)
     return (
       <p>
@@ -42,21 +53,23 @@ const ShowProject = () => {
   if (!project) return <p>No project found.</p>;
 
   return (
-    <div>
+    <>
       <Nav />
-      <div className=" align-middle bg-[#1b1b1b] flex-col text-white min-h-screen p-8">
-        <h1 className="text-3xl font-bold mb-4">{project.title}</h1>
-        <img
-          src={project.thumbURL}
-          alt={project.title}
-          className="max-w-full h-auto mb-4 rounded-lg"
-        />
-        <div
-          className="prose prose-invert"
-          dangerouslySetInnerHTML={{ __html: project.content }}
-        ></div>
+      <div className="flex items-center justify-center min-h-screen bg-[#1b1b1b] text-white p-8">
+        <div className="max-w-2xl text-left">
+          <h1 className="text-3xl font-bold mb-4">{project.title}</h1>
+          <img
+            src={project.thumbURL}
+            alt={project.title}
+            className="max-w-full h-auto mb-4 rounded-lg"
+          />
+          <div
+            className="prose prose-invert"
+            dangerouslySetInnerHTML={{ __html: project.content }}
+          ></div>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
