d34c4816d54136b968f2580fddb7f0b6b3c60e67
Paridhi Prisha Saikia <paridhiprishasaikia@gmail.com>
2026-02-26 14:42:26 +0530

    Updated resume

--- src/utils/downloadResume.jsx
+++ src/utils/downloadResume.jsx
@@ -1,7 +1,9 @@
+import replaceUrl from "./replaceUrl";
+
 const downloadResume = () => {
   const fileUrl =
-    "https://wdtlrkbrhenouwgibctr.supabase.co/storage/v1/object/public/resume/Paridhi%20Prisha%20Saikia%20Resume.pdf";
-  window.open(fileUrl, "_blank");
+    "https://wdtlrkbrhenouwgibctr.supabase.co/storage/v1/object/public/resume/Resume%20ATS%20Ver%2020260222.pdf";
+  window.open(replaceUrl(fileUrl), "_blank");
 };
 
 export default downloadResume;
